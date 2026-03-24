1f570be3cc9adbf878badb22853d3402a7c56c9d
newdevtoo <pguerisnor@gmail.com>
2026-03-24 19:12:00 -0400

    number

---
+++ you-are-the-casino/numjump.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var timer= Timer
+@onready var timmer = $life
+@onready var move = $move
+var move_speed = 40
+
+var color = 0
+@onready var numbers: Label = $numbers
+
+func _ready() -> void:
+	color = randi_range(0, 3)
+	if color == 0:
+		numbers.self_modulate = Color.YELLOW
+	elif color == 1:
+		numbers.self_modulate = Color.GREEN
+	else:
+		numbers.self_modulate = Color.BLUE
+	timmer.start()
+	if move.is_stopped():
+		move.start()
+	
+
+
+
+
+func _on_move_timeout() -> void:
+	position += Vector2.UP * move_speed
+	numbers.rotation += 60
+	move.start()
+
+
+func _on_life_timeout() -> void:
+	queue_free()
