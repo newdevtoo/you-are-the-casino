b37bdf63f9d9b78a28b1c70c47a89006ea2c08fa
newdevtoo <pguerisnor@gmail.com>
2026-03-28 19:43:28 -0400

    no comments

--- you-are-the-casino/numjump.gd
+++ you-are-the-casino/numjump.gd
@@ -9,6 +9,7 @@ var color = 0
 @onready var numbers: Label = $numbers
 
 func _ready() -> void:
+	numbers.text = str(Gamemanager.incomewithclick)
 	color = randi_range(0, 3)
 	if color == 0:
 		numbers.self_modulate = Color.YELLOW
@@ -26,7 +27,7 @@ func _ready() -> void:
 
 func _on_move_timeout() -> void:
 	position += Vector2.UP * move_speed
-	numbers.rotation += 60
+	numbers.rotation += 20
 	move.start()
 
 
